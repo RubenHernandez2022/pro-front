51c384f9b7c68d7e6306b64b1fc15ab45a6f4ad9
RubenHernandez2022 <ben_arg@hotmail.com>
2023-04-28 18:29:11 -0300

    proy front

--- src/components/Administrador/Administrador.js
+++ src/components/Administrador/Administrador.js
@@ -1,7 +1,7 @@
 import Navegation from "../Navegation/Navegation"; 
 import "./Administrador.css";
 
-import {useState} from"react";
+//import {useState} from"react";
 import{Fragment} from "react"
 
 
@@ -15,9 +15,9 @@ export default function Administrador(){
             <div class=" color-contacto d-flex flex-column justify-content-center align-items-center m-auto mt-5 w-75 h-75">
                 <h2>Producto nuevo</h2>
                 
-                <form className="row g-5 p-5" action="http://localhost:4000/Administrador" method="POST">
+                <form className="row g-5 p-5" action="http://localhost:4000/agregarProducto" method="POST">
                     <div className="col-12"> 
-                        <label htmlfor="tipoProducto" className="form-label">
+                        <label formHTML="tipoProducto" className="form-label">
                            Tipo de Producto  :
                            <select name="tipoProducto" id="tipoProducto" required>
                              <option value="Materiales">Materiales</option>
@@ -25,14 +25,15 @@ export default function Administrador(){
                              <option value="Repuestos">Repuestos</option>
                            </select>        
                         </label>
+                        {/* <input type="text" name="tipoProducto" className="form-control" id="tipoProducto"/> */}
                     </div> 
                     <div className="col-12">
-                       <label htmlFor="producto" className="form-label">Producto</label>
-                       <input type="text" className="form-control" id="producto"/>
+                       <label formHTML="producto" className="form-label">Producto</label>
+                       <input type="text" name="producto" className="form-control" id="producto"/>
                     </div>
                     <div className="col-12">
-                       <label htmlFor="marca" className="form-label">Marca</label>  
-                       <input type="text" className="form-control" id="marca"/>
+                       <label formHTML="marca" className="form-label">Marca</label>  
+                       <input type="text" name="marca" className="form-control" id="marca"/>
                     </div>
                     <div className="col-12">
                        <button type="=submit" className="btn boton-contacto">Enviar</button>
