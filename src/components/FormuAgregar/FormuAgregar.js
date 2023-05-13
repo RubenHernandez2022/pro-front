f188972fb96339a8671d19c666e1881065d50c05
RubenHernandez2022 <ben_arg@hotmail.com>
2023-05-13 00:57:47 -0300

    proy-front

---
+++ src/components/FormuAgregar/FormuAgregar.js
@@ -0,0 +1,37 @@
+
+
+export default function FormuAgregar(){
+    return(
+       <div className="container-fluid recuadro-contacto">
+            
+            <div class=" color-contacto d-flex flex-column justify-content-center align-items-center m-auto mt-5 w-75 h-75">
+                <h2>Producto nuevo</h2>
+                
+                <form className="row g-5 p-5" action="http://localhost:4000/agregarProducto" method="POST">
+                    <div className="col-12"> 
+                        <label formHTML="tipoProducto" className="form-label">
+                           Tipo de Producto  :
+                           <select name="tipoProducto" id="tipoProducto" required>
+                             <option value="Materiales">Materiales</option>
+                             <option value="Herramientas">Herramientas</option>
+                             <option value="Repuestos">Repuestos</option>
+                           </select>        
+                        </label>
+                        {/* <input type="text" name="tipoProducto" className="form-control" id="tipoProducto"/> */}
+                    </div> 
+                    <div className="col-12">
+                       <label formHTML="producto" className="form-label">Producto</label>
+                       <input type="text" name="producto" className="form-control" id="producto"/>
+                    </div>
+                    <div className="col-12">
+                       <label formHTML="marca" className="form-label">Marca</label>  
+                       <input type="text" name="marca" className="form-control" id="marca"/>
+                    </div>
+                    <div className="col-12">
+                       <button type="=submit" className="btn boton-contacto">Enviar</button>
+                    </div>
+                </form>
+            </div> 
+        </div> 
+    );
+} 
--- src/router/Router.js
+++ src/router/Router.js
@@ -2,6 +2,7 @@ import{BrowserRouter,Routes,Route} from "react-router-dom";
 import Home from "../components/Home/Home";
 import Cliente from "../components/Cliente/Cliente";
 import Administrador from "../components/Administrador/Administrador";
+import FormuAgregar from "../components/FormuAgregar/FormuAgregar";
 
 export default function Router(){
     return(<BrowserRouter>
@@ -9,6 +10,7 @@ export default function Router(){
                   <Route path="/" element={<Home/>}/>
                   <Route path="/Cliente" element={<Cliente/>}/>
                   <Route path="/Administrador" element={<Administrador/>}/>
+                  <Route path="/formuAgregar" element={<FormuAgregar/>}/>
                 </Routes>
             </BrowserRouter>
 
