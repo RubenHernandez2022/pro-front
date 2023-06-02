f8944fd4745afd770402b2c91146f94456207bd1
RubenHernandez2022 <ben_arg@hotmail.com>
2023-06-01 23:31:37 -0300

    proy-front

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -23,35 +23,31 @@ export default function Card({info}) {
   
   return(
     <Fragment>
-       {Ocultar ===true?
+      {Ocultar ===true?
           <div className="tarjeta d-flex flex-column m-2 "> 
               {/* <div className="d-flex flex-column aling-items-center p-2">
               {/* <button onClick={mostrar} className={Ocultar === true ?"d-flex align-self-end boton-show":"d-none"}>Comprar</button>   
               </div>*/}
               {/* <button className="cierre-menu d-flex align-self-end me-5 mt-5" onClick={ocultarInfo}>X</button> */}
-                        
               <div >
                   <div >
                       <img  className="imagen w-100 p-2"  src={info.imagen} alt={info.Imagen}/> 
                   </div>
-                  <div >
-                  <h2 className="d-flex align-self-center mt-2 color-name"><u>Artículo</u></h2>
-                  </div>
+                  <h2 className="d-flex justify-content-center mt-2 color-name"><u>Artículo</u></h2>
+                  
                   <ul >   
-                     <li className="color-name">Tipo Producto</li>
-                     <li className="lista-info ">{info.tipoProducto}</li>
-                     <li className="color-name">Producto</li>
-                     <li className="lista-info ">{info.Producto}</li>
-                     <li className="color-name">Marca</li>
-                     <li className="lista-info">{info.Marca}</li>
-                    
-                     <Link to="/Eliminar" activeClassName="active"><button className="btn btn-danger btn-eliminar" onClick={()=>eliminarProducto()}>Eliminar</button></Link>
+                    <li className="color-name">Tipo Producto</li>
+                    <li className="lista-info ">{info.tipoProducto}</li>
+                    <li className="color-name">Producto</li>
+                    <li className="lista-info ">{info.Producto}</li>
+                    <li className="color-name">Marca</li>
+                    <li className="lista-info">{info.Marca}</li>
+                    <Link to="/Eliminar" activeClassName="active"><button className="btn btn-danger btn-eliminar" onClick={()=>eliminarProducto()}>Eliminar</button></Link>
                   </ul>
               </div>
              
-           
           </div>:""
-       }
+      }
     </Fragment>  
   );
 
